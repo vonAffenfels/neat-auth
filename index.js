167f9d5c83d04f36d7e73ae241c666294b93a691
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2018-05-28 10:03:05 +0200

    wtf

--- index.js
+++ index.js
@@ -547,78 +547,83 @@ module.exports = class Auth extends Module {
         let selfModule = this;
 
         if (modelName === "user") {
-            schema.path('username').validate(function (value, cb) {
-                var self = this;
+            if (this.config.strategies && this.config.strategies.local === true) {
+                schema.path('username').validate(function (value, cb) {
+                    var self = this;
 
-                try {
-                    var regexp = new RegExp("^" + value + "$", 'i');
-                } catch (e) {
-                    cb(false);
-                }
-
-                Application.modules[selfModule.config.dbModuleName].getModel("user").findOne({
-                    username: regexp
-                }).read("primary").then((user) => {
-                    if ((user && user.id !== self.id)) {
-                        return cb(false);
-                    }
-                    cb(true);
-                }, () => {
-                    cb(false);
-                });
-            }, 'username duplicated');
-
-            schema.path('username').validate(function (value) {
-                if (value.length < 3) {
-                    return false;
-                }
-
-                return true;
-            }, 'username is too short 3-30');
-
-            schema.path('password').validate(function (value) {
-                if (value.length < 6) {
-                    return false;
-                }
-
-                return true;
-            }, 'password is too short, should be at least 6 characters long');
-
-            schema.path('username').validate(function (value) {
-                if (value.length > 30) {
-                    return false;
-                }
-
-                return true;
-            }, 'username is too long 3-30');
-
-            schema.path('username').validate(function (value) {
-                if (value.length === 0) {
-                    return false;
-                }
-
-                return true;
-            }, 'username is empty');
-
-            schema.path('email').validate(function (value, cb) {
-                var self = this;
-                try {
-                    var regexp = new RegExp("^" + value + "$", 'i');
-                } catch (e) {
-                    cb(false);
-                }
-                Application.modules[selfModule.config.dbModuleName].getModel("user").findOne({
-                    email: regexp
-                }).read("primary").then((user) => {
-                    if ((user && user.id !== self.id)) {
-                        return cb(false);
+                    try {
+                        var regexp = new RegExp("^" + value + "$", 'i');
+                    } catch (e) {
+                        cb(false);
                     }
 
-                    cb(true);
-                }, () => {
-                    return cb(false);
-                });
-            }, 'email duplicate');
+                    Application.modules[selfModule.config.dbModuleName].getModel("user").findOne({
+                        username: regexp
+                    }).read("primary").then((user) => {
+
+                        console.log(user.id, self.id);
+
+                        if ((user && user.id !== self.id)) {
+                            return cb(false);
+                        }
+                        cb(true);
+                    }, () => {
+                        cb(false);
+                    });
+                }, 'username duplicated');
+
+                schema.path('username').validate(function (value) {
+                    if (value.length < 3) {
+                        return false;
+                    }
+
+                    return true;
+                }, 'username is too short 3-30');
+
+                schema.path('password').validate(function (value) {
+                    if (value.length < 6) {
+                        return false;
+                    }
+
+                    return true;
+                }, 'password is too short, should be at least 6 characters long');
+
+                schema.path('username').validate(function (value) {
+                    if (value.length > 30) {
+                        return false;
+                    }
+
+                    return true;
+                }, 'username is too long 3-30');
+
+                schema.path('username').validate(function (value) {
+                    if (value.length === 0) {
+                        return false;
+                    }
+
+                    return true;
+                }, 'username is empty');
+
+                schema.path('email').validate(function (value, cb) {
+                    var self = this;
+                    try {
+                        var regexp = new RegExp("^" + value + "$", 'i');
+                    } catch (e) {
+                        cb(false);
+                    }
+                    Application.modules[selfModule.config.dbModuleName].getModel("user").findOne({
+                        email: regexp
+                    }).read("primary").then((user) => {
+                        if ((user && user.id !== self.id)) {
+                            return cb(false);
+                        }
+
+                        cb(true);
+                    }, () => {
+                        return cb(false);
+                    });
+                }, 'email duplicate');
+            }
 
             schema.path('termsAndConditions').validate(function (value, cb) {
                 if (!selfModule.config.enabled.terms) {
